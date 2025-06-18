3836f74f04cd7e989a1e3df286d8943bdb80b74b
drandule <sandal@yandex.ru>
2025-06-18 17:42:56 +0300

    ininitial

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#https://github.com/settings/tokens
+@app.get("/")
+def root():
+    return {"message": "Hello from katya"}
+
+@app.get("/alive")
+def alive():
+    return {"message": "Hello from alive"}
